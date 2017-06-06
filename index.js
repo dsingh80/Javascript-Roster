a564abf74967a2d4973c75990c6ad40b0c7c972c
="Damanveer Singh <=>
2017-06-06 13:58:10 -0400

    "Add Item" basic list functionality

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const addItemForm = document.querySelector('#addItemForm');
-const btnAddItem = document.querySelector('#addItemBtn');
+const foodList = document.querySelector('#foodList');
+
+let foods = [];
+
+function makeItem(foodName){
+
+    const newItem = document.createElement('li');
+
+    newItem.innerHTML = `
+        <div class="foodItem">
+            <p class="foodName">${foodName}</p>
+            <button class="btnDelete">Delete</button>
+            <button class="btnPromote">Promote</button>
+        </div>
+    `;
+
+    foodList.appendChild(newItem);
+    return newItem;
+}
 
 function addItem(ev){
-    
+    ev.preventDefault();
+
+    const foodName = addItemForm.foodName.value;
+    foods.push(makeItem(foodName));
+    console.log(foods);
+
 }
 
 

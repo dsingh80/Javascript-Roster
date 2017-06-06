109ac7c13cb463d2024e117815c75e792a3edee3
="Damanveer Singh <=>
2017-06-06 13:49:31 -0400

    Added Form and index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const addItemForm = document.querySelector('#addItemForm');
+const btnAddItem = document.querySelector('#addItemBtn');
+
+function addItem(ev){
+    
+}
+
+
+addItemForm.addEventListener('submit', addItem);
+
